58780001ed3bb54e0f45ad2e8864aaf9fc4a7c0c
KateBochaeva <117112623+KateBochaeva@users.noreply.github.com>
2023-03-19 20:33:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int result=0;
-  for (int i=0; i<size; i++) {
-    if (arr[i]==value)
-      result++;
+  int result = 0;
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == value)
+      result ++;
   }
-  if (result!=0)
+  if (result != 0)
     return result;
   else
     return 0; // если ничего не найдено
