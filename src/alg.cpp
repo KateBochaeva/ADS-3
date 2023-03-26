6624bbe3b64b0c862eb356d06e1f8eac19eebdbc
KateBochaeva <117112623+KateBochaeva@users.noreply.github.com>
2023-03-26 20:00:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,27 +1,26 @@
 // Copyright 2021 NNTU-CS
-int help(int *arr, int start, int end, int value, int quantity, int size){
+int help(int *arr, int start, int end, int value, int quantity){
   while (end>=start){
-    if (end>size || start<0){
-      return quantity;
-    }
     int midle=(end-start)/2;
+    if (middle==end || middle==start)
+      return quantity;
     if (arr[midle]==value){
       quantity++;
-      help(arr, start, midle-1, value, quantity,size);
-      help(arr, midle+1, end, value, quantity,size);
+      help(arr, start, midle-1, value, quantity);
+      help(arr, midle+1, end, value, quantity);
     }
     if (arr[midle]>value){
-      help(arr, start, midle-1, value, quantity,size);
+      help(arr, start, midle-1, value, quantity);
     }
     if (arr[midle]<value){
-      help(arr, midle+1, end, value, quantity,size);
+      help(arr, midle+1, end, value, quantity);
     }
   } 
   return quantity;
 }
 int cbinsearch(int *arr, int size, int value) {
   int quantity=0;
-  quantity=help(arr, 0, size, value, quantity,size);
+  quantity=help(arr, 0, size, value, quantity);
   if (quantity>0)
     return quantity;
   else
